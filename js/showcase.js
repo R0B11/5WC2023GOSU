fdcdab9f81feb57853e9b7303d3b6240e0ff141b
illonion <zhou.lawrence.yz@gmail.com>
2022-11-26 22:23:01 +1100

    Add Map Slot Functionality

--- js/showcase.js
+++ js/showcase.js
@@ -23,6 +23,7 @@ let currLENs
 let tempLENms;
 let tempLENs;
 let currBPM;
+let mapIds;
 
 let mapStatsCS = document.getElementById("mapStatsCSnum");
 let mapStatsAR = document.getElementById("mapStatsARnum");
@@ -45,6 +46,13 @@ let odbarPix;
 let csbarPercent;
 let csbarPix;
 
+let allMapSlots = document.getElementsByClassName("mapSlot");
+let currentMapSlot = 0;
+let mapSlotDifference;
+let toMapSlot = 0;
+let animTime;
+const delay = ms => new Promise(res => setTimeout(res, ms));
+
 let getMaps = new Promise(async (resolve, reject) => {
     let allMaps = getAllBeatmaps();
     console.log(getAllBeatmaps())
@@ -57,19 +65,16 @@ getMaps.then(allMaps => {
             newMapTitle.setAttribute("id", allMaps[i][j].beatmapID)
             newMapTitle.innerText = `${allMaps[i][j].mod.toUpperCase()}${allMaps[i][j].order}`
             
-            if (i == 0 && j == 0) newMapTitle.classList.add("aboveAll")
-            else if (i == 0 && j == 1) newMapTitle.classList.add("above")
-            else if (i == 0 && j == 2) newMapTitle.classList.add("current")
-            else if (i == 0 && j == 3) newMapTitle.classList.add("below")
+            if (i == 0 && j == 0) newMapTitle.classList.add("current")
+            else if (i == 0 && j == 1) newMapTitle.classList.add("below")
             else newMapTitle.classList.add("belowAll")
 
             currentMapWheel.append(newMapTitle)
         }
     }
 })
-let mapIds;
 
-socket.onmessage = event => {
+socket.onmessage = async event => {
     let data = JSON.parse(event.data);
     console.log(data)
 
@@ -87,6 +92,101 @@ socket.onmessage = event => {
         mapDifficulty.innerText = `[ ${data.menu.bm.metadata.difficulty} ]`;
         if (mapDifficulty.getBoundingClientRect().width > 324) mapDifficulty.classList.add("mapInfoWrap")
         else mapDifficulty.classList.remove("mapInfoWrap")
+
+        // Map Slot Scrolling
+        // Calculate toMapSlot
+        for (var i = 0; i < allMapSlots.length; i++) {
+            if (allMapSlots[i].id == mapID) {
+                toMapSlot = i;
+                break;
+            }
+        }
+        // Calculate difference in number of slots
+        mapSlotDifference = Math.abs(currentMapSlot - toMapSlot)
+        // Remove All Animation Classes
+        function removeAnimationClasses() {
+            new Promise((res) => {
+                for (var j = 0; j < allMapSlots.length; j++) {
+                    allMapSlots[j].classList.remove("fromAboveAllToAbove");
+                    allMapSlots[j].classList.remove("fromAboveToCurrent");
+                    allMapSlots[j].classList.remove("fromCurrentToBelow");
+                    allMapSlots[j].classList.remove("fromBelowToBelowAll");
+                    allMapSlots[j].classList.remove("fromBelowAllToBelow");
+                    allMapSlots[j].classList.remove("fromBelowToCurrent");
+                    allMapSlots[j].classList.remove("fromCurrentToAbove");
+                    allMapSlots[j].classList.remove("fromAboveToAboveAll");
+                }
+                res(allMapSlots)
+            })
+        }
+        // Going Down
+        if (currentMapSlot < toMapSlot) {
+        
+            for (var i = 0; i < mapSlotDifference; i++) {
+                await removeAnimationClasses();
+                animTime = Math.round(1000 / Math.abs(currentMapSlot - toMapSlot))
+                if (document.body.contains(allMapSlots[currentMapSlot - 1])) {
+                    allMapSlots[currentMapSlot - 1].classList.remove("above")
+                    allMapSlots[currentMapSlot - 1].classList.add("fromAboveToAboveAll")
+                    allMapSlots[currentMapSlot - 1].classList.add("aboveAll")
+                    allMapSlots[currentMapSlot - 1].style.animationDuration = `${animTime}ms`
+                }
+
+                allMapSlots[currentMapSlot].classList.remove("current")
+                allMapSlots[currentMapSlot].classList.add("fromCurrentToAbove")
+                allMapSlots[currentMapSlot].classList.add("above")
+                allMapSlots[currentMapSlot].style.animationDuration = `${animTime}ms`
+
+                if (document.body.contains(allMapSlots[currentMapSlot + 1])) {
+                    allMapSlots[currentMapSlot + 1].classList.remove("below")
+                    allMapSlots[currentMapSlot + 1].classList.add("fromBelowToCurrent")
+                    allMapSlots[currentMapSlot + 1].classList.add("current")
+                    allMapSlots[currentMapSlot + 1].style.animationDuration = `${animTime}ms`
+                }
+
+                if (document.body.contains(allMapSlots[currentMapSlot + 2])) {
+                    allMapSlots[currentMapSlot + 2].classList.remove("belowAll")
+                    allMapSlots[currentMapSlot + 2].classList.add("fromBelowAllToBelow")
+                    allMapSlots[currentMapSlot + 2].classList.add("below")
+                    allMapSlots[currentMapSlot + 2].style.animationDuration = `${animTime}ms`
+                }
+                await delay(animTime)
+                currentMapSlot++
+            }
+        } else {
+            for (var i = 0; i < mapSlotDifference; i++) {
+                await removeAnimationClasses()
+                animTime = 1000 / Math.abs(currentMapSlot - toMapSlot)
+                if (document.body.contains(allMapSlots[currentMapSlot - 2])) {
+                    allMapSlots[currentMapSlot - 2].classList.remove("aboveAll")
+                    allMapSlots[currentMapSlot - 2].classList.add("fromAboveAllToAbove")
+                    allMapSlots[currentMapSlot - 2].classList.add("above")
+                    allMapSlots[currentMapSlot - 2].style.animationDuration = `${animTime}ms`
+                }
+
+                if (document.body.contains(allMapSlots[currentMapSlot - 1])) {
+                    allMapSlots[currentMapSlot - 1].classList.remove("above")
+                    allMapSlots[currentMapSlot - 1].classList.add("fromAboveToCurrent")
+                    allMapSlots[currentMapSlot - 1].classList.add("current")
+                    allMapSlots[currentMapSlot - 1].style.animationDuration = `${animTime}ms`
+                }
+
+                allMapSlots[currentMapSlot].classList.remove("current")
+                allMapSlots[currentMapSlot].classList.add("fromCurrentToBelow")
+                allMapSlots[currentMapSlot].classList.add("below")
+                allMapSlots[currentMapSlot].style.animationDuration = `${animTime}ms`
+
+                if (document.body.contains(allMapSlots[currentMapSlot + 1])) {
+                    allMapSlots[currentMapSlot + 1].classList.remove("below")
+                    allMapSlots[currentMapSlot + 1].classList.add("fromBelowToBelowAll")
+                    allMapSlots[currentMapSlot + 1].classList.add("belowAll")
+                    allMapSlots[currentMapSlot + 1].style.animationDuration = `${animTime}ms`
+                }
+
+                await delay(animTime)
+                currentMapSlot--
+            }
+        }
     }
 
     // SR, CS, AR, OD
