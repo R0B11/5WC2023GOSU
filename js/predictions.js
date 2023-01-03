c94993cfdbd535b5cc533177b188004e651572be
ROB_ <114362409+R0B11@users.noreply.github.com>
2023-01-03 05:47:23 -0500

    Button Functionality, Cycles through Countries

---
+++ js/predictions.js
@@ -0,0 +1,28 @@
+
+const predictionTeams = ["Argentina","Australia","Austria","Belgium","Brazil","Canada","Chile","China","Colombia","Czech Republic","Denmark", "Estonia","Finland","France","Germany","Hong Kong","Indonesia","Israel","Italy","Kazakhstan","Latvia","Lithuania","Malaysia","Mexico","Nepal","Netherlands","Peru", "Philippines", "Poland","Portugal","Réunion", "Romania","Russian Federation", "Singapore","Slovakia","South Korea", "Spain", "Sweden", "Taiwan", "Thailand", "Turkey", "Ukraine", "United Kingdom", "United States", "Vietnam"];
+
+let currentTeamLocation = 0
+let setTeamName = document.getElementById("currentTeamName");
+let setTeamFlag = document.getElementById("currentTeamFlag")
+
+setTeamName.innerText = predictionTeams[currentTeamLocation];
+setTeamFlag.style.backgroundImage = `url("static/flags/${predictionTeams[currentTeamLocation]}.png")`;
+
+
+
+function teamDisplayleft(){
+
+    currentTeamLocation = currentTeamLocation - 1;
+    if (currentTeamLocation < 0) {currentTeamLocation = predictionTeams.length};
+    setTeamName.innerText = predictionTeams[currentTeamLocation];
+    setTeamFlag.style.backgroundImage = `url("../static/flags/${predictionTeams[currentTeamLocation]}.png")`;
+
+}
+function teamDisplayright(){
+    
+    currentTeamLocation = currentTeamLocation + 1;
+    if (currentTeamLocation > (predictionTeams.length - 1)) {currentTeamLocation = 0};
+    setTeamName.innerText = predictionTeams[currentTeamLocation];
+    setTeamFlag.style.backgroundImage = `url("../static/flags/${predictionTeams[currentTeamLocation]}.png")`;
+
+}
