0667963e4b8bb5a7413c5999f857e3ac08cf4b99
ROB_ <114362409+R0B11@users.noreply.github.com>
2023-02-18 23:35:07 -0500

    GitHub Fix

--- js/showcase.js
+++ js/showcase.js
@@ -25,8 +25,7 @@ let tempLENs;
 let currBPM;
 let mapIds;
 
-let replayer;
-
+let mapFound = false
 let mapStatsCS = document.getElementById("mapStatsCSnum");
 let mapStatsAR = document.getElementById("mapStatsARnum");
 let mapStatsOD = document.getElementById("mapStatsODnum");
@@ -38,8 +37,6 @@ let srBar = document.getElementById("mapStatsSRbar");
 let arBar = document.getElementById("mapStatsARbar");
 let csBar = document.getElementById("mapStatsCSbar");
 let odBar = document.getElementById("mapStatsODbar");
-
-let replayerPanel = document.getElementById("replayer");
 
 let srbarPercent;
 let srbarPix;
@@ -57,83 +54,46 @@ let toMapSlot = 0;
 let animTime;
 const delay = ms => new Promise(res => setTimeout(res, ms));
 
-let allMaps = JSON.parse(`[
-    {
-      "beatmapTitle": "Relive",
-      "poolerComment": "Concistency check on aim",
-      "modID": "NM1"
-    },
-    {
-      "beatmapTitle": "We Got It (feat. Rothwell)",
-      "poolerComment": "Flow aim & stamina tapping",
-      "modID": "NM2"
-    },
-    {
-      "beatmapTitle": "firewall",
-      "poolerComment": "Alt with a little bit of tech tapping",
-      "modID": "NM3"
-    },
-    {
-      "beatmapTitle": "Hestia",
-      "poolerComment": "Slidertech aim",
-      "modID": "NM4"
-    },
-    {
-      "beatmapTitle": "Zange Mairi",
-      "poolerComment": "Flow and stacks tapping",
-      "modID": "HD1"
-    },
-    {
-      "beatmapTitle": "My Life Is Over and I Couldn't Be Happier",
-      "poolerComment": "170BPM aim, light reading",
-      "modID": "HD2"
-    },
-    {
-      "beatmapTitle": "Edge",
-      "poolerComment": "Tapping and aim tech mix",
-      "modID": "HR1"
-    },
-    {
-      "beatmapTitle": "So Cold",
-      "poolerComment": "110BPM Aim",
-      "modID": "HR2"
-    },
-    {
-      "beatmapTitle": "Kono Sekai wa Bokura wo Matteita",
-      "poolerComment": "Aim with some triples, AR10",
-      "modID": "DT1"
-    },
-    {
-      "beatmapTitle": "Extend Ash ~ Houraijin",
-      "poolerComment": "Speed tapping",
-      "modID": "DT2"
-    },
-    {
-      "beatmapTitle": "PANTA RHEI",
-      "poolerComment": "Aim focus with a good amount of streams",
-      "modID": "FM1"
+let poolerComment
+let poolComment = document.getElementById("comments");
+
+let replayerName
+let replayer = document.getElementById("replayer")
+let allMaps;
+
+let getMaps = new Promise(async (resolve, reject) => {
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET",`http://localhost:24050/5WC2023GOSU/data/showcaseBeatmaps.json`, false);
+    xhr.onload = function xhrLoad()  {
+        if (this.status == 404) return
+        if (this.status == 200) allMaps = JSON.parse(this.responseText)
     }
-  ]`);
+    xhr.send();
+    resolve(allMaps); 
+})
+getMaps.then(allMaps => {
+    for (var i = 0; i < allMaps.length; i++) {
+        let newMapTitle = document.createElement("div")
+        console.log(allMaps[i])
+        let newSongName = allMaps[i].songName.replace(" ","_")
+        let newSongDifficulty = allMaps[i].difficulty.replace(" ","_")
+        newMapTitle.setAttribute("id", `${newSongName}_${newSongDifficulty}`)
+        newMapTitle.innerText = `${allMaps[i].modid.toUpperCase()}`
+        newMapTitle.classList.add("mapSlot")
 
-for (var i = 0; i < allMaps.length; i++) {
-    let newMapTitle = document.createElement("div")
-    newMapTitle.setAttribute("id", allMaps[i].beatmapID)
-    newMapTitle.innerText = `${allMaps[i].modID.toUpperCase()}`
-    newMapTitle.classList.add("mapSlot")
+        if (i == 0) newMapTitle.classList.add("current")
+        else if (i == 1) newMapTitle.classList.add("below")
+        else newMapTitle.classList.add("belowAll")
 
-    if (i == 0) newMapTitle.classList.add("current")
-    else if (i == 1) newMapTitle.classList.add("below")
-    else newMapTitle.classList.add("belowAll")
-
-    currentMapWheel.append(newMapTitle)
-}
+        currentMapWheel.append(newMapTitle)
+    }
+})
 
 socket.onmessage = async event => {
     let data = JSON.parse(event.data);
     // Now Playing Container Data
     if (mapID !== data.menu.bm.id) {
-        console.log(data.menu.bm.id)
-        console.log(mapID)
+        mapFound = false
         mapID = data.menu.bm.id;
         // Map Image
         currImg = data.menu.bm.path.full.replace(/#/g,'%23').replace(/%/g,'%25').replace(/\\/g,'/');
@@ -147,16 +107,52 @@ socket.onmessage = async event => {
         if (mapDifficulty.getBoundingClientRect().width > 324) mapDifficulty.classList.add("mapInfoWrap")
         else mapDifficulty.classList.remove("mapInfoWrap")
 
-        // Replayer: Identify and inject
-        replayer = data.gameplay.name;
-        replayerPanel.innerHTML = replayer;
-
         // Map Slot Scrolling
         // Calculate toMapSlot
         for (var i = 0; i < allMapSlots.length; i++) {
-            if (allMapSlots[i].id == mapID) {
+            let currentSongName = data.menu.bm.metadata.title.replace(" ","_")
+            let currentSongDifficulty = data.menu.bm.metadata.difficulty.replace(" ", "_")
+            if (allMapSlots[i].id == `${currentSongName}_${currentSongDifficulty}`) {
                 toMapSlot = i;
-                console.log(toMapSlot)
+                mapFound = true
+                
+                // SR
+                currSR = allMaps[i].sr
+                currSR = Math.round(currSR * 100) / 100
+                srbarPercent = (currSR - 4) / 3.1
+                srbarPix = srbarPercent * 352
+                srBar.style.width = `${srbarPix}px`
+                mapStatsSR.innerText = currSR
+                // CS
+                currCS = allMaps[i].cs
+                currCS = Math.round(currCS * 10) / 10
+                csbarPercent = (currCS - 2) / 5.15
+                csbarPix = csbarPercent * 352
+                csBar.style.width = `${csbarPix}px`
+                mapStatsCS.innerText = currCS
+                // AR
+                currAR = allMaps[i].ar
+                currAR = Math.round(currAR * 10) / 10
+                arbarPercent = (currAR - 5) / 5.2
+                arbarPix = arbarPercent * 352
+                arBar.style.width = `${arbarPix}px`
+                mapStatsAR.innerText = currAR
+                // OD
+                currOD = allMaps[i].od
+                currOD = Math.round(currOD * 10) / 10
+                odbarPercent = (currOD - 5) / 5
+                odbarPix = odbarPercent * 352
+                odBar.style.width = `${odbarPix}px`
+                mapStatsOD.innerText = currOD
+                // LEN
+                tempLENs = allMaps[i].len
+                currLENs = `${(Math.floor(tempLENs / 60))}:${("0" + Math.floor(tempLENs % 60)).slice(-2)}`;
+                mapStatsLEN.innerText = currLENs;
+                // BPM
+                currBPM = allMaps[i].bpm
+                mapStatsBPM.innerText = currBPM
+                // Pooler Comment
+                poolComment.innerText = allMaps[i].comment
                 break;
             }
         }
@@ -249,62 +245,69 @@ socket.onmessage = async event => {
     }
 
     // SR, CS, AR, OD
-    if (currSR !== data.menu.bm.stats.fullSR) {
-        currSR = data.menu.bm.stats.fullSR;
-        mapStatsSR.innerText = currSR;
-        if (currSR > 4) {
-            srbarPercent = (currSR - 4) / 3.1;
-            srbarPix = srbarPercent * 352;
-            srBar.style.width = `${srbarPix}px`;
+    if (!mapFound) {
+        if (currSR !== data.menu.bm.stats.fullSR) {
+            currSR = data.menu.bm.stats.fullSR;
+            mapStatsSR.innerText = currSR;
+            if (currSR > 4) {
+                srbarPercent = (currSR - 4) / 3.1;
+                srbarPix = srbarPercent * 352;
+                srBar.style.width = `${srbarPix}px`;
+            }
+            else srBar.style.width = `0px`;
         }
-        else {
-            srBar.style.width = `0px`;
+        if (currCS !== data.menu.bm.stats.CS) {
+            currCS = data.menu.bm.stats.CS;
+            mapStatsCS.innerText = currCS;
+            csbarPercent = (currCS - 2) / 5.15;
+            csbarPix = csbarPercent * 352;
+            csBar.style.width = `${csbarPix}px`;
         }
-    }
-    if (currCS !== data.menu.bm.stats.CS) {
-        currCS = data.menu.bm.stats.CS;
-        mapStatsCS.innerText = currCS;
-        csbarPercent = (currCS - 2) / 5.15;
-        csbarPix = csbarPercent * 352;
-        csBar.style.width = `${csbarPix}px`;
-    }
-    if (currAR !== data.menu.bm.stats.AR) {
-        currAR = data.menu.bm.stats.AR;
-        mapStatsAR.innerText = currAR;
-        arbarPercent = (currAR - 5) / 5.2;
-        arbarPix = arbarPercent * 352;
-        arBar.style.width = `${arbarPix}px`;
-    }
-    if (currOD !== data.menu.bm.stats.OD) {
-        currOD = data.menu.bm.stats.OD
-        mapStatsOD.innerText = currOD;
-        odbarPercent = (currOD - 5) / 5;
-        odbarPix = odbarPercent * 352;
-        odBar.style.width = `${odbarPix}px`;
-    }
-
-    // Length
-    if (currLENms !== data.menu.bm.time.full) {
-        // Get Time in MS
-        currLENms = data.menu.bm.time.full;
-        tempLENms = (data.menu.mods.str.includes("DT")) ? currLENms / 3 * 2 : currLENms;
-        // Get Time in S and apply
-        tempLENs = tempLENms / 1000;
-        currLENs = `${(Math.floor(tempLENs / 60))}:${("0" + Math.floor(tempLENs % 60)).slice(-2)}`;
-        mapStatsLEN.innerText = currLENs;
-    }
-
-    // BPM
-    if (Math.abs(data.menu.bm.stats.BPM.min - data.menu.bm.stats.BPM.max) !== 0){ 
-        if (currBPM !== `${data.menu.bm.stats.BPM.min} - ${data.menu.bm.stats.BPM.max}`) {
-            currBPM = `${data.menu.bm.stats.BPM.min} - ${data.menu.bm.stats.BPM.max}`;
+        if (currAR !== data.menu.bm.stats.AR) {
+            currAR = data.menu.bm.stats.AR;
+            mapStatsAR.innerText = currAR;
+            arbarPercent = (currAR - 5) / 5.2;
+            arbarPix = arbarPercent * 352;
+            arBar.style.width = `${arbarPix}px`;
+        }
+        if (currOD !== data.menu.bm.stats.OD) {
+            currOD = data.menu.bm.stats.OD
+            mapStatsOD.innerText = currOD;
+            odbarPercent = (currOD - 5) / 5;
+            odbarPix = odbarPercent * 352;
+            odBar.style.width = `${odbarPix}px`;
+        }
+    
+        // Length
+        if (currLENms !== data.menu.bm.time.full) {
+            // Get Time in MS
+            currLENms = data.menu.bm.time.full;
+            tempLENms = (data.menu.mods.str.includes("DT")) ? currLENms / 3 * 2 : currLENms;
+            // Get Time in S and apply
+            tempLENs = tempLENms / 1000;
+            currLENs = `${(Math.floor(tempLENs / 60))}:${("0" + Math.floor(tempLENs % 60)).slice(-2)}`;
+            mapStatsLEN.innerText = currLENs;
+        }
+        // BPM
+        if (Math.abs(data.menu.bm.stats.BPM.min - data.menu.bm.stats.BPM.max) !== 0){ 
+            if (currBPM !== `${data.menu.bm.stats.BPM.min} - ${data.menu.bm.stats.BPM.max}`) {
+                currBPM = `${data.menu.bm.stats.BPM.min} - ${data.menu.bm.stats.BPM.max}`;
+                mapStatsBPM.innerText = currBPM;
+                console.log(currBPM !== `${data.menu.bm.stats.BPM.min} - ${data.menu.bm.stats.BPM.max}`);
+            }
+        } 
+        else if (currBPM !== data.menu.bm.stats.BPM.min) {
+            currBPM = data.menu.bm.stats.BPM.min;
             mapStatsBPM.innerText = currBPM;
-            console.log(currBPM !== `${data.menu.bm.stats.BPM.min} - ${data.menu.bm.stats.BPM.max}`);
         }
-    } 
-    else if (currBPM !== data.menu.bm.stats.BPM.min) {
-        currBPM = data.menu.bm.stats.BPM.min;
-        mapStatsBPM.innerText = currBPM;
     }
 
+    // Replayer Name
+    if (replayerName !== data.gameplay.name) {
+        replayerName = data.gameplay.name
+        replayer.innerText = replayerName
+    }
+
+    
+    
 }
